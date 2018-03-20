ad9e5999c687ea9408f1c8be6f4a533aa6b80ef0
Nijish <nijishvk@gmail.com>
2018-03-20 09:15:28 +0530

    demo version 1
    demo version 1

--- angular/src/app/layout/dashboard/dashboard.module.ts
+++ angular/src/app/layout/dashboard/dashboard.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgbCarouselModule, NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
+import { ChartsModule as Ng2Charts } from 'ng2-charts';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
@@ -17,6 +18,7 @@ import { StatModule } from '../../shared';
         NgbCarouselModule.forRoot(),
         NgbAlertModule.forRoot(),
         DashboardRoutingModule,
+        Ng2Charts,
         StatModule
     ],
     declarations: [
